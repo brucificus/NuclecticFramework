1d6c4dd7ec4f42713cf8287ff1ab5dc43d72ee98
Bruce Markham <illuminus86@gmail.com>
2014-03-22 01:20:57 -0400

    no cross-wiring or DI yet, but it builds and runs

---
+++ PortableGameTest.Core/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PortableGameTest.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ PortableGameTest.Framework.Win32/Support/IO/FileSystemAdapter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortableGameTest.Framework.Support.IO
+{
+    public class FileSystemAdapter
+    {
+    }
+}
