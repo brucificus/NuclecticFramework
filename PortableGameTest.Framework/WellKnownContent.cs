d34e904846c489bb5c28fed17516eee509cded91
Bruce Markham <illuminus86@gmail.com>
2014-08-05 18:14:08 -0400

    trying to figure out where content fits into the framework, since we can't do EmbeddedResources like Nuclex used to

---
+++ PortableGameTest.Framework/WellKnownContent.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PortableGameTest.Framework
+{
+	public static class WellKnownContent
+	{
+		public static Effect ScreenMaskEffect(ContentManager contentManager)
+		{
+			return contentManager.Load<Effect>("ScreenMaskEffect");
+		}
+
+		public static Effect DefaultTextEffect(ContentManager contentManager)
+		{
+			return contentManager.Load<Effect>("DefaultTextEffect");
+		}
+
+		public static Effect SolidColorEffect(ContentManager contentManager)
+		{
+			return contentManager.Load<Effect>("SolidColorEffect");
+		}
+
+		public static class Skins
+		{
+			public static class Suave
+			{
+				public static SpriteFont DefaultFont(ContentManager contentManager)
+				{
+					return contentManager.Load<SpriteFont>("Skins/Suave/DefaultFont");
+				}
+
+				public static Texture2D SuaveSheet(ContentManager contentManager)
+				{
+					return contentManager.Load<Texture2D>("Skins/Suave/SuaveSheet");
+				}
+
+				public static SpriteFont TitleFont(ContentManager contentManager)
+				{
+					return contentManager.Load<SpriteFont>("Skins/Suave/TitleFont");
+				}
+			}
+		}
+	}
+}
