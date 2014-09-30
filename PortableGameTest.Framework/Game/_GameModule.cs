0cd5b89bc39411989a5d9c404f7c6a8f62172c09
Bruce Markham <illuminus86@gmail.com>
2014-09-30 15:19:10 -0400

    more division, deleted the incomplete tree stuff

--- PortableGameTest.Framework/Game/_GameModule.cs
+++ PortableGameTest.Framework/Game/_GameModule.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 using Autofac;
 using Nuclex.Game.Content;
-using Nuclex.Game.Packing;
 using Nuclex.Game.States;
 using PortableGameTest.Framework.Game.States;
 
@@ -17,10 +16,6 @@ namespace PortableGameTest.Framework.Game
         protected override void Load(ContainerBuilder builder)
         {
             builder.RegisterType<SharedContentManager>().As<ISharedContentService>().SingleInstance();
-
-            builder.RegisterType<ArevaloRectanglePacker>().As<RectanglePacker>().InstancePerDependency();
-            builder.RegisterType<CygonRectanglePacker>().As<RectanglePacker>().InstancePerDependency();
-            builder.RegisterType<SimpleRectanglePacker>().As<RectanglePacker>().InstancePerDependency();
 
 	        builder.RegisterType<ManualGameStateManager>().AsSelf().As<IManualGameStateService>().SingleInstance();
 			builder.RegisterType<AutoGameStateManager>().AsImplementedInterfaces().AsSelf().SingleInstance();
