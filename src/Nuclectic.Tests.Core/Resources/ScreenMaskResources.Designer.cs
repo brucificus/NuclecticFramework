7def2b622ccafd95d2aab009397620c461511fb0
Bruce Markham <illuminus86@gmail.com>
2014-10-08 14:27:59 -0400

    bringing in Nuclex's original fx files, modified to work against DX11 HLSL, which makes more unit tests pass

--- src/Nuclectic.Tests.Core/Resources/ScreenMaskResources.Designer.cs
+++ src/Nuclectic.Tests.Core/Resources/ScreenMaskResources.Designer.cs
@@ -59,5 +59,15 @@ namespace Nuclectic.Tests.Resources {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] ScreenMaskEffect {
+            get {
+                object obj = ResourceManager.GetObject("ScreenMaskEffect", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
     }
 }
